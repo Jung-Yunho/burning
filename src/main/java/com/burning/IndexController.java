433ccc51bc72afec595f9423d7e3353156a146c0
Yunho <boyjcy@naver.com>
2022-05-29 10:39:50 +0900

    기본적인 프로젝트 세팅

---
+++ src/main/java/com/burning/IndexController.java
@@ -0,0 +1,20 @@
+package com.burning;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping("/yh")
+    public String indexYh() {
+        return "yh/hello.html";
+    }
+}
