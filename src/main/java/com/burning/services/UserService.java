4c8a2a5de4c909ca1609910457d3796fbc0498c6
김동희 <kdhgla@gmail.com>
2022-06-28 22:09:59 +0900

    비밀번호 저장시 sha-256 적용

--- src/main/java/com/burning/services/UserService.java
+++ src/main/java/com/burning/services/UserService.java
@@ -8,6 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,8 +29,21 @@ public class UserService {
     }
 
     @Transactional
-    public Long signUp(UserRequestDto userResponseDto) {
+    public Long signUp(UserRequestDto userResponseDto) throws NoSuchAlgorithmException {
+        userResponseDto.setPwd1(encrypt(userResponseDto.getPwd1()));
         return userRepository.save(userResponseDto.toEntity()).getId();
     }
 
+    public static String encrypt(String str) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1)
+                sb.append('0');
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
 }
