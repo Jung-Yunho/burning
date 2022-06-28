7df726baed7e9375faf471b96d84475b9b9a8602
김동희 <kdhgla@gmail.com>
2022-06-28 22:09:10 +0900

    로그인아이디 영문 숫자만 가능하게 수정

--- src/main/java/com/burning/web/dto/UserRequestDto.java
+++ src/main/java/com/burning/web/dto/UserRequestDto.java
@@ -15,8 +15,8 @@ import javax.validation.constraints.Size;
 @Setter
 @NoArgsConstructor
 public class UserRequestDto {
-    @NotBlank(message = "로그인 아이디는 필수 입력 값입니다.")
-    @Size(min = 8, max = 16, message = "로그인아이디는 8자 이상 16자 이하로 입력해주세요.")
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Pattern(regexp = "[a-zA-Z0-9]{8,16}", message = "아이디는 영문, 숫자만 가능하며 8 ~ 16자리까지 가능합니다.")
     private String loginID;
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
